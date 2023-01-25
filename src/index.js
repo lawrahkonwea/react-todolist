b0c2b42fa12504558ac639781a80eb21f3cdae2e
Amaka Konwea <lawrahcollections@gmail.com>
2023-01-25 21:57:21 +0000

    Implemented the input form and styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
+import "./App.css"
 import TodoContainer from "./components/TodoContainer"
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
 
